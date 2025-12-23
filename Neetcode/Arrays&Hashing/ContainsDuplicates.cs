3477b6cdf47b96f2bcfd95659d9b05451aff44e9
Anas <anas_ansar1:outlook.com>
2025-12-23 20:20:02 +0000

    Able to check for duplicates using nested for loops. Time and space complexities for each method noted.

--- Neetcode/Arrays&Hashing/ContainsDuplicates.cs
+++ Neetcode/Arrays&Hashing/ContainsDuplicates.cs
@@ -1,5 +1,32 @@
-public class Solution {
-    public bool hasDuplicate(int[] nums) {
+using System;
+using System.Linq;
+
+public class ContainsDuplicates {
+    /**
+    Nested For Loops (brute force approach)
+    Best case: Linear complexity O(n)
+    Worst case: Quadratic complexity O(n²)
+    Space complexity: Constant O(1)
+    **/
+    public bool usingNestedLoops(int[] nums) {
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+    LINQ GroupBy 
+    Best case: Constant complexity O(1)
+    Average case: Linear complexity O(n)
+    Worst case: Quadratic complexity O(n²) - extremely rare as this means all items are the same
+    Space complexity: Linear O(n)
+    **/
+    public bool usingGroupBy(int[] nums) {
         if (nums.GroupBy(r=>r).Any(g=>g.Count()>1)){
             return true;
         }
