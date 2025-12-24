db57f887f677e35236f1f0f52d9cc5da0b6232ad
Anas <anas_ansar1:outlook.com>
2025-12-24 19:23:04 +0000

    Added appropriate comments and solved for duplicates using Hash Sets

--- Neetcode/Arrays&Hashing/ContainsDuplicates.cs
+++ Neetcode/Arrays&Hashing/ContainsDuplicates.cs
@@ -1,6 +1,16 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
+/// <summary>
+/// Contains Duplicates
+/// https://neetcode.io/practice/contains-duplicates
+/// Given a list of numbers, I am using different methods to check for duplicates.
+/// I am also trying to find the time and space complexity of each method.
+/// Current ranking:
+/// 1. Hash sets & LINQ GroupBy (tied)
+/// 2. Nested For Loops
+/// </summary>
 public class ContainsDuplicates {
     /**
     Nested For Loops (brute force approach)
@@ -29,6 +39,24 @@ public class ContainsDuplicates {
     public bool usingGroupBy(int[] nums) {
         if (nums.GroupBy(r=>r).Any(g=>g.Count()>1)){
             return true;
+        }
+        return false;
+    }
+
+    /**
+    Hash sets:
+    Best & Average case: Constant O(1)
+    Worst case: Linear O(n) all items have hash collisions/are the same
+    Space complexity: Linear O(n)
+    **/
+    public bool usingHashSet(int[] nums)
+    {
+        HashSet<int> seen = new HashSet<int>();
+        foreach (int num in nums) {
+            if (seen.Contains(num)) {
+                return true;
+            }
+            seen.Add(num);
         }
         return false;
     }
