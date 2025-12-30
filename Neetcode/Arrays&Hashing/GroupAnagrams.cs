727ebef1f530fb3aca3b77ea8b7b3fbf5e5f063f
Anas <anas_ansar1@outlook.com>
2025-12-30 19:11:44 +0000

    Solved group anagrams using frequency based anagram detection.

--- Neetcode/Arrays&Hashing/GroupAnagrams.cs
+++ Neetcode/Arrays&Hashing/GroupAnagrams.cs
@@ -4,7 +4,8 @@
 /// An Anagram is a word or phrase formed by rearranging the letters of a different word
 /// than the original word.
 /// Ranking:    
-/// 1. Sorting Approach - O(n * k log k) time, O(n) space
+/// 1. Without Sorting Approach - O(n * k) time, O(n) space
+/// 2. Sorting Approach - O(n * k log k) time, O(n) space
 /// </summary>
 public class GroupAnagrams {
     string[] strs = new string[] {"eat", "tea", "tan", "ate", "nat", "bat"};
@@ -30,10 +31,44 @@ public class GroupAnagrams {
                 // If yes, add the value to the key
                 allAnagrams[sorted] = new List<string>();
             }
-            // If no, create a new key which is the sorted string, and add the actual word to the list of values
+            // If no, create a new key which is the sorted string, and add the actual word to the list of values
             allAnagrams[sorted].Add(word);
         }
 
-        return new List<List<string>>(allAnagrams.Values);
-   }
+        return new List<List<string>>(allAnagrams.Values);
+    }
+
+    /**
+    Without Sorting
+    Time complexity: O(n * k) where n is the number of strings and k is the maximum length of a string in strs
+    Space complexity: O(n)
+    **/
+    public List<List<string>> NoSorting()
+    {
+        // Create a dictionary to store all anagrams
+        var allAnagrams = new Dictionary<string, List<string>>();
+
+        foreach (string word in strs)
+        {
+            // Create frequency array for 26 letters
+            int[] charCount = new int[26];
+
+            // Count each letter in the word
+            foreach (char c in word)
+            {
+                charCount[c - 'a']++;
+            }
+
+            // Convert frequency array to unique key    
+            string key = string.Join("#", charCount);
+
+            if (!allAnagrams.ContainsKey(key))
+            {
+                allAnagrams[key] = new List<string>();
+            }
+            allAnagrams[key].Add(word);
+        }
+
+        return new List<List<string>>(allAnagrams.Values);
+    }
 }
